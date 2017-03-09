440081c37ca2d3eb599b83c9a5b9298da4b33632
LarryChuksGitHub <larrychuks08@gmail.com>
2017-03-09 17:26:09 +0100

    Update zip_to_ipa.sh

--- zip_to_ipa.sh
+++ zip_to_ipa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /Users/hchukwuji/Documents/workspace/ios-projects/xctest-xcuitest-project/project/app/builds/Build/Products/Release-iphoneos/
+cd /project/app/builds/Build/Products/Release-iphoneos/
 mkdir -p ./temporary/Payload
 echo "created temporary folder"
 cd temporary/Payload
-cp -r /Users/hchukwuji/Documents/workspace/ios-projects/xctest-xcuitest-project/project/app/builds/Build/Products/Release-iphoneos/calculator.app .
+cp -r /project/app/builds/Build/Products/Release-iphoneos/calculator.app .
 cd ..
 echo "creating .ipa file it may take some time"
 rm -rf .ipa
